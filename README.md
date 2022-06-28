090b5a564850e3b13ba0bad4c04d20f57bb91611
MeriemP <meriem0319@gmail.com>
2022-06-27 20:58:45 -0400

    fixed some JS and HTML and CSS and did some README

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Color-Mixing-Quiz
+# Color-Mixing-Quiz Application
+
+## My Task
+
+This Challenge is asking me to build a timed quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that I write. It will have a clean, polished, and responsive user interface. 
+
+## User Story
+
+```
+As an artist who loves to paint 
+I want to take a timed quiz about mixing paint colors
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN I am alerted that the time is up
+WHEN the game is over
+THEN I can save my initials AND be shown my score
+```
+
+## ScreenGIF
+
+
+## Deployment 
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,9 @@ var infoSlide = document.querySelector(".info-slide");
 var exitBtn = infoSlide.querySelector(".buttons .quit");
 var continueBtn = infoSlide.querySelector(".buttons .next");
 var quizSlide = document.querySelector(".quiz-slide");
+var timerCount = quizSlide.querySelector(".timer .timer-sec");
+var answerList = document.querySelector(".answer-list");
+
 
 // add click listener if start button is clicked then show the quiz rules
 startBtn.onclick = ()=>{
@@ -20,6 +23,8 @@ exitBtn.onclick = ()=>{
 continueBtn.onclick = ()=>{
     infoSlide.classList.remove("activeInfo"); //hide the info
     quizSlide.classList.add("activeQuiz");  //then show the first quiz
+    displayQuestions(0);
+    startTimer(15);
 }
 
 // Need to list the questions in the quiz 
@@ -67,11 +72,112 @@ var myQuestions = [
 ]
 
 var questCount = 0;
+var counter;
+var timeValue = 15;
+var userScore = 0;
+
+
+var nextBtn = quizSlide.querySelector(".next-question");
+var resultSlide = document.querySelector(".result-slide");
+var recordInitials = resultSlide.querySelector(".buttons .next");
+var quitQuiz = resultSlide.querySelector(".buttons .quit");
+
+//at the end of the quiz, the result container buttons will either reload page to restart quiz, or click to record initals
+
+quitQuiz.onclick = ()=> {
+    window.location.reload();
+}
+
+recordInitials.onclick = ()=> {
+    var initials = window.prompt("Enter your Initials:", "Initials"); 
+    if (initials != null) {
+        document.getElementById("initials").innerHTML = 
+        "Thank you for playing " + initials + "! Your score is " + userScore;
+    }
+}
+
+// if the next question button is clicked, use if loop to display next question 
+nextBtn.onclick = ()=> {
+    if (questCount < myQuestions.length - 1) {
+         questCount++;
+         displayQuestions(questCount);
+         nextBtn.style.display = "none";        
+    } else {        
+         showResult();
+    }    
+}
+
 
 // Set up the questions and answers using the array above 
-// function displayQuestions(){
-//     var questionText = document.querySelector(".question-text");
-    
-// }
+function displayQuestions(index){
+    var questionText = document.querySelector(".question-text");    
+    var questionTag = '<span>' + myQuestions[index].question +'<span>';
+    var answerTag = '<div class="answer">' + myQuestions[index].options[0] + '<span></span></div>'
+                    + '<div class="answer">' + myQuestions[index].options[1] + '<span></span></div>'
+                    + '<div class="answer">' + myQuestions[index].options[2] + '<span></span></div>';
+    questionText.innerHTML = questionTag;
+    answerList.innerHTML = answerTag;
+
+    var answer = answerList.querySelectorAll(".answer");
+    for (let i = 0; i < answer.length; i++) {
+        answer[i].setAttribute("onclick", "answerSelected(this)");
+    }
+}
+
+// adding an icon to the right (palette)/wrong (xmark) answers 
+
+var paletteIcon = '<div class="icon palette"><i class="fa-solid fa-palette"></i></div>';
+
+var xmarkIcon = '<div class="icon xmark"><i class="fa-solid fa-circle-xmark"></i></div>';
+
+// indicate the wrong from right answers and show by color if wrong/right 
+function answerSelected(ans) {          
+    var userAnswer = ans.textContent;
+    var corrAnswer = myQuestions[questCount].answer;
+    var allAnswers = answerList.children.length;    
+    if (userAnswer === corrAnswer) {
+        ans.classList.add("corr");
+        userScore += 1;
+        ans.insertAdjacentHTML("beforeend", paletteIcon);
+    } else {        
+        ans.classList.add("incorr");                                   
+        ans.insertAdjacentHTML("beforeend", xmarkIcon);
+        time = time - 5;      
+    }
+    for (let i = 0; i < allAnswers; i++) {        
+        answerList.children[i].classList.add("disabled");        
+    }    
+    nextBtn.style.display = "block";        
+}
 
 
+function showResult() {
+    clearInterval(counter);
+    infoSlide.classList.remove("activeInfo"); //hide the info
+    quizSlide.classList.remove("activeQuiz");  //then hide quiz
+    resultSlide.classList.add("activeResult"); //show the results
+    var scoreText = resultSlide.querySelector(".score-text");
+    if (userScore > 0) {
+        let scoreTag = '<span>Your Score is: <p>' + userScore + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if (userScore === 0) {        
+            let scoreTag = '<span>Sorry, you did not score any points!</span>';
+            scoreText.innerHTML = scoreTag;            
+    }
+}
+
+// adding a function for the timer 
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    function timer() {
+        timerCount.textContent = time;
+        time--;        
+        if (time < 0) {
+            showResult();
+            clearInterval(counter);
+            alert("Time is Up!");
+            timerCount.textContent = "00";                      
+        }  
+    }     
+}
